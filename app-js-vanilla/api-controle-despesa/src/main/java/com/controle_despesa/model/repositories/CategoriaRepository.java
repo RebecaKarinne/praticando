b961b2a7734cdfd52d8cc9d51b69ed70e1e59f94
RebecaKarinne <rebeca.karinne@gmail.com>
2021-11-11 23:02:45 -0300

    Exemplo de repository (substituto do DAO)

---
+++ app-js-vanilla/api-controle-despesa/src/main/java/com/controle_despesa/model/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.controle_despesa.model.repositories;
+
+import com.controle_despesa.model.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    // No CrudRepository tem outros métodos, como o save, findById, findAll... 
+
+
+
+}
