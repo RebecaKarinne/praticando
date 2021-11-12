5eca93d5204b1a8023905a5b340c5b7787267f0e
RebecaKarinne <rebeca.karinne@gmail.com>
2021-11-11 22:51:39 -0300

    Usando o lombok - criando Getters e Setters automagicamente

---
+++ app-js-vanilla/api-controle-despesa/src/main/java/com/controle_despesa/model/entity/Categoria.java
@@ -0,0 +1,17 @@
+package com.controle_despesa.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria {
+
+    private Long id;
+    private String descricao;
+
+}
